2dd4248c614fd269f7845c4ff43b3feadfe0f7da
MattHerringshaw1 <matt.herringshaw@yahoo.com>
2022-10-05 20:18:13 -0500

    Red or Blue

---
+++ ClassWork/Week4/Day2/red_blue/js/app.js
@@ -0,0 +1,11 @@
+
+const submitButton1 = document.getElementById("submitButton1")
+const submitButton2 = document.getElementById("submitButton2")
+
+submitButton1.addEventListener("click", function() {
+    document.body.style.backgroundColor = "red"
+})
+
+submitButton2.addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue"
+})
