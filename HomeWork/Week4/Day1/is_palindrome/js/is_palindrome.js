912d70cdd03bf2b6da600b22f196a74d23783d93
MattHerringshaw1 <matt.herringshaw@yahoo.com>
2022-10-09 14:09:33 -0500

    palindrome

--- HomeWork/Week4/Day1/is_palindrome/js/is_palindrome.js
+++ HomeWork/Week4/Day1/is_palindrome/js/is_palindrome.js
@@ -1,9 +1,9 @@
 
-let test = "mom"
+let word = "moan"
 
 function isPalindrome(word) {
     let reverse = ""
-    for(let index = word.length - 1; index ; index--) {
+    for(let index = word.length - 1; index >=0 ; index--) {
         reverse += word[index]
     }
     if (reverse === word) {
@@ -12,26 +12,7 @@ function isPalindrome(word) {
     else {
         console.log("Not Palindrome")
     }
+    
 }
 
-
-
-// isPalindrome(test)
-
-
-// def is_palindrome(str):
-// reversed_word = ""
-// for index in range(len(str) - 1, -1, -1):
-//     reversed_word += str[index]
-
-// if reversed_word.lower() == str.lower():
-//     return True
-// else:
-//     return False
-
-
-// str = input("Enter a word here: ")
-// if is_palindrome(str):
-// print("Palindrome")
-// else:
-// print("Not Palindrome")
+isPalindrome(word)
