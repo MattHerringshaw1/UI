5b91ae98e2f4ca4497ae7b01bd72d5513af7ee59
MattHerringshaw1 <matt.herringshaw@yahoo.com>
2022-10-09 14:22:36 -0500

    fizz buzz

--- HomeWork/Week4/Day1/fizz_buzz/fizz_buzz.js
+++ HomeWork/Week4/Day1/fizz_buzz/fizz_buzz.js
@@ -1,6 +1,19 @@
 
+function fizzBuzz(number) {
+    if (number % 3 == 0 && number % 5 == 0) {
+        console.log("Fizz Buzz")
+    }
+    else if (number % 3 == 0) {
+        console.log("Fizz")
+    }
+    else if (number % 5 == 0) { 
+        console.log("Buzz")
+    }
+}
 
-
+fizzBuzz(9)
+fizzBuzz(15)
+fizzBuzz(10)
 
 // number = int(input("Enter a number: "))
 // if (number % 3) ==0 and (number % 5) == 0:
